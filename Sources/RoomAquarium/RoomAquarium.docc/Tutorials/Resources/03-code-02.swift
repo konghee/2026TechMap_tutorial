d8766f9284cf9111c97d683174b502e19c130ad1
Jeong Hee <konghee0411@gmail.com>
2026-09-07 01:53:35 +0900

    Add comment for tracking session in AquariumView
    Added a comment to explain the use of @State for tracking session.

--- Sources/RoomAquarium/RoomAquarium.docc/Tutorials/Resources/03-code-02.swift
+++ Sources/RoomAquarium/RoomAquarium.docc/Tutorials/Resources/03-code-02.swift
@@ -3,6 +3,7 @@ import RealityKit
 import AquariumContent
 
 struct AquariumView: View {
+    // 화면이 유지되는 동안 같은 트래킹 세션을 사용하기 위해 @State로 저장합니다.
     @State private var trackingSession = SpatialTrackingSession()
 
     var body: some View {
